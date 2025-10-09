dc646fb44474cd8afbde2f6e610f526d4ab10c05
RenzGP <enzoprepose@gmail.com>
2025-10-09 11:14:10 +0800

    Added PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withPWA({
+  dest: 'public', // Where the service worker and manifest are generated
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // disable PWA in dev mode
+})({
+  reactStrictMode: true,
+});
 
 export default nextConfig;
