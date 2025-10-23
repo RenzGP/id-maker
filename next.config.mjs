d30c70bc6fce6f5776ea6521beb600b6069dbfcb
RenzGP <enzoprepose@gmail.com>
2025-10-24 01:26:19 +0800

    Fix PWA Service worker for offline support

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,17 @@
 import withPWA from 'next-pwa';
 
-/** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  // Optional: Vercel optimization
+  output: 'standalone',
+};
+
+export default withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-  fallbacks: {
-    document: '/offline.html', // optional offline fallback page
-  },
-})({
-  reactStrictMode: true,
-});
-
-export default nextConfig;
+})(nextConfig);
